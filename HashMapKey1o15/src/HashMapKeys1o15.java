296de76de6f7edfd1374176edbb192669af2d673
Radhika <Radhika@DESKTOP-S0V7M4T>
2022-09-12 17:11:44 +0530

    Print Square of Key as its values

---
+++ HashMapKey1o15/src/HashMapKeys1o15.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+public class HashMapKeys1o15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 HashMap<String, Integer> hash_map = new HashMap<String, Integer>();
+		  
+	      for(int i=1; i<=15; i++) {
+	    	  
+	      }
+	     
+	        hash_map.put("Geeks", 11);
+	        hash_map.put("4", 12);
+	        hash_map.put("Geeks", 21);
+	        hash_map.put("Welcomes", 25);
+	        hash_map.put("You", 40);
+	  
+	        // Displaying the HashMap
+	        System.out.println("Initial Mappings are: " + hash_map);
+	  
+	        System.out.println("The collection is: " + hash_map.values());
+		
+		
+	}
+
+}
